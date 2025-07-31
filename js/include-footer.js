5e1c446944369c458127c50a1852e32959c89558
Valentin Bacout <80823004+valentinbacout@users.noreply.github.com>
2025-07-31 08:52:33 +0200

    Create include-footer.js

---
+++ js/include-footer.js
@@ -0,0 +1,8 @@
+fetch('footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  })
+  .catch(error => {
+    console.error('Erreur lors du chargement du footer:', error);
+  });
