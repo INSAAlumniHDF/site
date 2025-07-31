448bbedc05f590f5dcac59724d8467ac8cfc1940
Valentin Bacout <80823004+valentinbacout@users.noreply.github.com>
2025-07-31 18:30:18 +0200

    Delete js/include-header.js

--- js/include-header.js
+++
@@ -1,7 +0,0 @@
-// js/include-header.js
-fetch('header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  })
-  .catch(error => console.error('Erreur lors du chargement du header :', error));
