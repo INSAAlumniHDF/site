5312215e93ca6d56986d1f7158afcc40ce215ea0
Valentin Bacout <80823004+valentinbacout@users.noreply.github.com>
2025-08-19 09:24:53 +0200

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,14 +1,29 @@
-const CACHE_NAME = 'site-cache-v1';
+const CACHE_NAME = 'insa-alumni-cache-v1';
 const urlsToCache = [
+  '',
   'index.html',
   'styles.css',
-  '/js/script.js',
+  'js/script.js',
+  'img/logo.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames
+          .filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      )
+    ).then(() => self.clients.claim())
   );
 });
 
@@ -16,5 +31,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
+      .catch(() => caches.match('index.html'))
   );
 });
